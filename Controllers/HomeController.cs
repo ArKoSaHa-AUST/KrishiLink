f679b34f9e7f497cc774935a5bd95556eecc83b8
ArKo SaHa <arkosaha61005@gmail.com>
2026-08-28 21:54:22 +0600

    feat(ui): integrate platform logo, redesign landing page, and refine status badges

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,11 +4,17 @@ namespace KrishiLink.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        [Route("landingpage")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("Privacy")]
+        [Route("Home/Privacy")]
         public IActionResult Privacy()
         {
             return View();
